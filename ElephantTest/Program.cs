5b9ee1a3cbacccf1c71e287ab4753bffcb0a0e83
cozz-py <bencozzens13@gmail.com>
2023-02-21 07:41:13 -0500

    Add project files.

---
+++ ElephantTest/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class Elephant
+{
+    public int earsize;
+    public string name;
+
+    public void WhoAmI()
+    {
+        Console.WriteLine("My name is " + name + ".");
+        Console.WriteLine("My ears are " + earsize + " inches tall.");
+    }
+}
